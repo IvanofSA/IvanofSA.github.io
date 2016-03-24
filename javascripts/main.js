176c3199ebe40a1ea8463be6f6fe72f2bd4dfbdc
Ivanov Sergey <poshelya@gmail.com>
2016-03-24 20:55:33 +0300

    first dz

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,21 @@
-(function(){
-  var accordion = document.getElementById('accordion'),
-      titles = accordion.getElementsByTagName('div');
-
-  for ( var i=0; i<titles.length; i++ ){
-    titles[i].addEventListener('click', displayAccordion);
-  }
-
-  function displayAccordion() {
-    var content = this.nextSibling.nextSibling;
-    console.log(this)
-
-    if ( !content.classList.contains('active') ) {
-      content.classList.add('active')
-      this.classList.add('color')
-    } else {
-      content.classList.remove('active')
-      this.classList.remove('color')
-    }
-  }
-})();
+// (function(){
+//   var accordion = document.getElementById('accordion'),
+//       titles = accordion.getElementsByTagName('div');
+//
+//   for ( var i=0; i<titles.length; i++ ){
+//     titles[i].addEventListener('click', displayAccordion);
+//   }
+//
+//   function displayAccordion() {
+//     var content = this.nextSibling.nextSibling;
+//     console.log(this)
+//
+//     if ( !content.classList.contains('active') ) {
+//       content.classList.add('active')
+//       this.classList.add('color')
+//     } else {
+//       content.classList.remove('active')
+//       this.classList.remove('color')
+//     }
+//   }
+// })();
