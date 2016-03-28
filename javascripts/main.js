4ecbaa98c56327ee4f2b198ceb01bea51af3e0ff
Ivanov Sergey <poshelya@gmail.com>
2016-03-28 23:42:44 +0300

    hw1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,94 @@
-// (function(){
-//   var accordion = document.getElementById('accordion'),
-//       titles = accordion.getElementsByTagName('div');
+new Promise(function(resolve) {
+    if (document.readyState === 'complete') {
+        resolve();
+    } else {
+        window.onload = resolve;
+    }
+})
+    .then(function() {
+        return new Promise(function(resolve, reject) {
+            VK.init({
+                apiId: 5377462
+            });
+            VK.Auth.login(function(response) {
+                if (response.session) {
+                    resolve(response);
+                } else {
+                    reject(new Error('Не удалось авторизоваться'));
+                }
+            }, 2);
+        });
+    })
+    .then(function() {
+
+      function allowDrop(e) {
+        e.preventDefault();
+      }
+
+      function drag(e) {
+        e.dataTransfer.setData("text", e.target.id);
+        // console.log(e.target.id);
+      }
+
+      function drop(e) {
+        e.preventDefault();
+        var data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.getElementById(data));
+        // console.log(e.target.id);
+        console.log(document.getElementById(data));
+
+      }
+      
+    return new Promise(function(resolve, reject) {
+        VK.api('friends.get', {'fields':"photo"}, function(response) {
+          console.log(response);
+            if (response.error) {
+                reject(new Error(response.error.error_msg));
+            } else {
+                var source = playerItemTemplate.innerHTML,
+                    templateFn = Handlebars.compile(source),
+                    template = templateFn({list: response.response});
+
+                friendAll.innerHTML = template;
+
+                resolve();
+            }
+        });
+    });
+}).catch(function(e) {
+    alert('Ошибка: ' + e.message);
+});
+
+
+
+
+
+
+
+
+// .then(function(cityArr){
+// 	search__all.addEventListener('keyup', addList);
 //
-//   for ( var i=0; i<titles.length; i++ ){
-//     titles[i].addEventListener('click', displayAccordion);
-//   }
+// 	function addList(e) {
+// 		console.log(this);
+// 		var townFilter = cityArr.filter(filterFn, this);
+// 		console.log(townFilter);
+// 		block.innerHTML = '';
 //
-//   function displayAccordion() {
-//     var content = this.nextSibling.nextSibling;
-//     console.log(this)
+// 		for (var i = 0; i < townFilter.length; i++) {
+// 			var newLi = document.createElement('li');
+// 			newLi.innerHTML = townFilter[i];
+// 			block.appendChild(newLi);
+// 		}
+// 	}
 //
-//     if ( !content.classList.contains('active') ) {
-//       content.classList.add('active')
-//       this.classList.add('color')
-//     } else {
-//       content.classList.remove('active')
-//       this.classList.remove('color')
-//     }
-//   }
-// })();
+// 	function filterFn(el, ind, ar, thisArg) {
+// 		var inpValue  = this.value;
+// 		console.log(this.value);
+// 		if (inpValue === '') {
+// 			return false;
+// 		}
+// 		return el.indexOf(inpValue) !== -1;
+// 	}
+//
+// })
