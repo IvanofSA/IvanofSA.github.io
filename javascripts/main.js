a69cf5df6b24ae1917b0e9517f7b0f2e09f4061a
Ivanov Sergey <poshelya@gmail.com>
2016-03-30 01:32:42 +0300

    dnd + add и remove

--- javascripts/main.js
+++ javascripts/main.js
@@ -32,23 +32,30 @@ new Promise(function(resolve) {
                     template = templateFn({list: response.response});
 
                 friendAll.innerHTML = template;
-
             }
-
 
             function handleDragStart(e) {
-              e.dataTransfer.setData('text', e.target.id);
-
-            }
+              e.target.closest("li").setAttribute("id","transferElem");
+              e.dataTransfer.setData('text', e.target.closest("li").id);
+            };
             function handleDragOver(e) {
               if (e.preventDefault) {
                 e.preventDefault();
-
               }
-              // e.dataTransfer.dropEffect = 'move';
-              //
-              // return false;
-            }
+            };
+            function handleDragEnd(e) {
+              e.target.closest("li").removeAttribute("id");
+              var changePlus = e.target.querySelector('[data-role=plus]');
+              console.log(e.target.querySelectorAll('.avatar'));
+
+              if (changePlus.getAttribute('data-role') === 'plus') {
+                changePlus.setAttribute('data-role', 'minus');
+                e.target.closest("li").setAttribute('draggable', 'false');
+                changePlus.className = 'glyphicon glyphicon-minus';
+              }
+
+            };
+
             function handleDrop(e) {
               if (e.stopPropagation) {
                 e.stopPropagation();
@@ -56,22 +63,39 @@ new Promise(function(resolve) {
 
               e.preventDefault();
               var data = e.dataTransfer.getData("text");
-              friendListGroup.appendChild(document.getElementById(data));
+              friendSave.appendChild(document.getElementById(data));
+            };
 
-            }
-
-
-            var cols = document.querySelectorAll('.list-group-item');
+            var cols = document.querySelectorAll('.friendItem');
 
             [].forEach.call(cols, function(col) {
-
               col.addEventListener('dragstart', handleDragStart, false);
-
             });
 
-            friendListGroup.addEventListener('drop', handleDrop, false);
-            friendListGroup.addEventListener('dragover', handleDragOver, false);
+            friendSave.addEventListener('drop', handleDrop, false);
+            friendSave.addEventListener('dragover', handleDragOver, false);
+            friendSave.addEventListener('dragend', handleDragEnd, false);
+            friendWrap.addEventListener('click', transferFriend, false);
 
+
+            function transferFriend(e) {
+              var elemRemove = e.target.closest("li");
+              var el = friendList.querySelector('.friend');
+                console.log(el);
+                if (e.target.getAttribute('data-role') === 'plus') {
+                  elemRemove.setAttribute('draggable', 'false');
+                  e.target.setAttribute('data-role', 'minus');
+                  e.target.className = 'glyphicon glyphicon-minus';
+                  friendSave.appendChild(elemRemove);
+
+                } else if(e.target.getAttribute('data-role') === 'minus') {
+                    elemRemove.setAttribute('draggable', 'true');
+                    e.target.setAttribute('data-role', 'plus');
+                    e.target.className = 'glyphicon glyphicon-plus';
+                    friendList.appendChild(elemRemove);
+                  }
+
+            }
         });
 
     });
