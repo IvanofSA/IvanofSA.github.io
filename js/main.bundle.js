12289e6f190ca5460dde4edf3d99936a83702dc0
Poshelya <poshelya@gmail.com>
2018-02-01 15:20:07 +0300

    ajax

--- js/main.bundle.js
+++ js/main.bundle.js
@@ -1,22 +1,23 @@
 var file = document.getElementById('img');
+var submit = document.getElementById('submit');
+var baseImg = '';
 
+submit.addEventListener('click', sendAjaxRequest);
 file.addEventListener('change', previewFile);
 
 function previewFile() {
   var file = document.querySelector('input[type=file]').files[0];
-  var reader  = new FileReader();
+  var reader = new FileReader();
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  var centerX = canvas.offsetWidth / 2;
-  var centerY = canvas.offsetHeight / 2;
 
   reader.onloadend = function () {
+    baseImg = reader.result;
     var image = new Image();
-    image.onload = function() {
+    image.onload = function () {
       ctx.drawImage(image, 0, 0);
     };
-    image.src = reader.result;
-
+    image.src = baseImg;
   };
 
   if (file) {
@@ -24,3 +25,12 @@ function previewFile() {
   }
 }
 
+
+function sendAjaxRequest(e) {
+  e.preventDefault();
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/url/url/url', true);
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.send(baseImg);
+}
