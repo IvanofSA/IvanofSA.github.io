00e4ae933b621a5332880d16a2aec41a4df9d4f1
Poshelya <poshelya@gmail.com>
2018-02-13 13:16:25 +0300

    crop img

--- js/main.bundle.js
+++ js/main.bundle.js
@@ -1,21 +1,22 @@
 var file = document.getElementById('img');
 var submit = document.getElementById('submit');
 var baseImg = '';
-
-submit.addEventListener('click', sendAjaxRequest);
 file.addEventListener('change', previewFile);
+var cropper;
 
 function previewFile() {
   var file = document.querySelector('input[type=file]').files[0];
   var reader = new FileReader();
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
+  var image = document.getElementById('image');
 
   reader.onloadend = function () {
     baseImg = reader.result;
-    var image = new Image();
     image.onload = function () {
-      ctx.drawImage(image, 0, 0);
+      if (image.complete) {
+        start.call(image);
+      } else {
+        image.onload = start;
+      }
     };
     image.src = baseImg;
   };
@@ -25,12 +26,29 @@ function previewFile() {
   }
 }
 
+function start() {
+  var width = this.offsetWidth;
+  var height = this.offsetHeight;
+  canvas.width = width;
+  canvas.height = height;
+  canvas.getContext('2d').drawImage(
+    this,
+    0, 0, this.naturalWidth, this.naturalHeight,
+    0, 0, width, height
+  );
+  cropper = new Cropper(canvas);
 
-function sendAjaxRequest(e) {
+}
+
+submit.addEventListener('click', function (e) {
   e.preventDefault();
+  var cropImg = cropper.getCroppedCanvas().toDataURL('image/jpeg');
+  console.log(cropper.getCroppedCanvas().toDataURL('image/jpeg'));
 
   var xhr = new XMLHttpRequest();
   xhr.open('POST', '/url/url/url', true);
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  xhr.send(baseImg);
-}
+  xhr.send(cropImg);
+
+});
+
